c9164628ae773d916d8d561370b1a96e28780c88
Benjamin Guillet (Tiné) <benjamin.guillet@utbm.fr>
2011-11-25 22:43:28 +0100

    Modif classe GameView et ajout images, sons...

--- AngryUTBM/src/GameView.java
+++ AngryUTBM/src/GameView.java
@@ -1,4 +1,22 @@
-public class GameView {
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 
-	private String field;
+public class GameView extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	public GameView() {
+		this.setTitle("AngryUTBM");
+		this.setSize(800, 600);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		
+		ImagePanel bgImage = new ImagePanel(new ImageIcon("res/images/background.png").getImage());      
+		
+		this.getContentPane().add(bgImage);
+		this.pack();		
+		
+		this.setVisible(true);
+	}
+	
 }
