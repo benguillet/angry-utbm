1ce66aff15c03d22f62158c8ec5c9d336821dde9
Benjamin Guillet (Tiné) <benjamin.guillet@utbm.fr>
2011-12-09 17:11:34 +0100

    Collisions + thread pig

--- AngryUTBM/src/Pig.java
+++ AngryUTBM/src/Pig.java
@@ -15,10 +15,38 @@ public class Pig extends Enemy {
     	ImageIcon ii = new ImageIcon(imagePath);
 	    image = ii.getImage();
 	    position = new Position(100, 500);
-	    speed = 30;
+	    speed = 1;
 	    frameSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
 	    hitBox = new Rectangle(new Point(position.getX(),position.getY()), new Dimension(50, 50));
 	}
+	
+	public void run() {
+		move();
+	}
+
+	public void move() {
+		boolean back = false;
+		
+		while(true) {
+			if (position.getX() > 800 - image.getWidth(null)) back = true;
+			if (position.getX() < 1) back = false;
+
+			
+			if (!back)
+				position.setX(position.getX() + speed);
+			else
+		    	position.setX(position.getX() - speed);
+			
+			 try {
+                 Thread.sleep(15);
+			 } catch (InterruptedException e) {
+                 e.printStackTrace();
+			 }
+			
+			
+		}
+	}
+	
 	public Rectangle getBound() {
 		// TODO Auto-generated method stub
 		
