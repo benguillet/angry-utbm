a77244dc2c527c343f801ef8ae5040d65c7869b8
YuShin <theo.madet@gmail.com>
2011-12-11 01:41:48 +0100

    Correction bug déplacement cochon

--- AngryUTBM/src/Pig.java
+++ AngryUTBM/src/Pig.java
@@ -8,35 +8,38 @@ public class Pig extends Enemy {
 
 	private Dimension frameSize;
 	private Rectangle hitBox;
-	
-	
+	private boolean back;
+
 	public Pig() {
-	    imagePath = "res/images/pig.png";
-    	ImageIcon ii = new ImageIcon(imagePath);
-	    image = ii.getImage();
-	    position = new Point(100, 500);
-	    speed = 1;
-	    frameSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-	    hitBox = new Rectangle(new Point((int)position.getX(),(int)position.getY()), new Dimension(50, 50));
+		imagePath = "res/images/pig.png";
+		ImageIcon ii = new ImageIcon(imagePath);
+		image = ii.getImage();
+		position = new Point(100, 500);
+		speed = 1;
+		back = false;
+		frameSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+		hitBox = new Rectangle(new Point((int) position.getX(),
+				(int) position.getY()), new Dimension(50, 50));
 	}
 
-public void move() {
-	boolean back = false;
-		
-	if (position.getX() > 800 - image.getWidth(null)) back = true;
-	if (position.getX() < 1) back = false;
-		
-	if (!back)
-		position.setLocation(position.getX() + speed, position.getY());
-	else
-	   	position.setLocation(position.getX() - speed, position.getY());
-			 
-		hitBox.setBounds((int)position.getX(),(int)position.getY(),50,50);
-		
+	public void move() {
+
+		if (position.getX() > 800 - image.getWidth(null))
+			back = true;
+		if (position.getX() < 1)
+			back = false;
+
+		if (!back)
+			position.setLocation(position.getX() + speed, position.getY());
+		else
+			position.setLocation(position.getX() - speed, position.getY());
+
+		hitBox.setBounds((int) position.getX(), (int) position.getY(), 50, 50);
+
 	}
-	
+
 	public Rectangle getBound() {
 		return hitBox;
 	}
-	
+
 }
