d421682007fe18ef3d7fe04cf028a7112832defa
YuShin <theo.madet@gmail.com>
2011-12-11 02:26:31 +0100

    Correction classe Player

--- AngryUTBM/src/Player.java
+++ AngryUTBM/src/Player.java
@@ -1,9 +1,6 @@
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
