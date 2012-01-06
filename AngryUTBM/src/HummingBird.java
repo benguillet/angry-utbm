881e95b10e120a521927aeeb4ef34434126ef731
YuShin <theo.madet@gmail.com>
2012-01-06 23:12:20 +0100

    Ajout d'un temps de vol maximum (10 sec)

--- AngryUTBM/src/HummingBird.java
+++ AngryUTBM/src/HummingBird.java
@@ -5,18 +5,10 @@ import javax.swing.ImageIcon;
 
 public class HummingBird extends Bird {
 	private final int PIGEON_SIZE = 58;
-	private double accel = 9.81;
-	private double time = 0.1;
-	private boolean isMoving= false;
-	private double angle = 45;
-	private Dimension frameSize;
-	private int startLocationX = 40;
-	private int startLocationY = 400;
-	private double accelX = 0;
-	private boolean isFlying = false;
+	
 	
 	public HummingBird() {
-	    imagePath = "res/images/hummingBird.png";
+	    imagePath = "res/images/hummingbird.png";
     	ImageIcon ii = new ImageIcon(imagePath);
 	    image = ii.getImage();
 	    position = new Point(startLocationX, startLocationY);
@@ -25,37 +17,6 @@ public class HummingBird extends Bird {
 	    reload();
 	    frameSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
 	}
-
-    public void move() {
-    	if(isMoving)
-    	{
-	    	position.setLocation(speed*Math.cos(angle)*time+0.5*accelX*time*time+startLocationX,0.5*accel*time*time-Math.sin(angle)*speed*time+startLocationY);
-	    	time+=0.1;
-	    	if (position.getY() > (int) frameSize.getHeight() || position.getX() > (int) frameSize.getWidth())
-				visible = false;
-    	}
-    }
-    
-    public void launch(){
-    	isMoving = true;
-    	isFlying = true;
-    }
-    
-    public boolean getFlyingState()
-    {
-    	return isFlying;
-    }
-    
-	
-	public void moveRight() {
-		//position.setLocation(position.getX() + speed, position.getY());
-		accelX+=0.1;
-    }
-    
-    public void moveLeft() {
-    	//position.setLocation(position.getX() - speed, position.getY());
-    	accelX-=0.1;
-    }
     
     public void moveUp() {
     	//position.setLocation(position.getX() ,position.getY() - speed);
--- AngryUTBM/src/Pigeon.java
+++ AngryUTBM/src/Pigeon.java
@@ -5,14 +5,6 @@ import javax.swing.ImageIcon;
 
 public class Pigeon extends Bird {
 	private final int PIGEON_SIZE = 58;
-	private double accel = 9.81;
-	private double time = 0.1;
-	private boolean isMoving= false;
-	private double angle = 45;
-	private Dimension frameSize;
-	private int startLocationX = 40;
-	private int startLocationY = 400;
-	private double accelX = 0;
 	
 	
 	public Pigeon() {
@@ -25,32 +17,6 @@ public class Pigeon extends Bird {
 	    reload();
 	    frameSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
 	}
-
-    public void move() {
-    	if(isMoving)
-    	{
-	    	position.setLocation(speed*Math.cos(angle)*time+0.5*accelX*time*time+startLocationX,0.5*accel*time*time-Math.sin(angle)*speed*time+startLocationY);
-	    	time+=0.1;
-	    	if (position.getY() > (int) frameSize.getHeight() || position.getX() > (int) frameSize.getWidth())
-				visible = false;
-    	}
-    }
-    
-    public void launch(){
-    	isMoving = true;
-    	isFlying = true;
-    }
-    
-	
-	public void moveRight() {
-		//position.setLocation(position.getX() + speed, position.getY());
-		accelX+=0.1;
-    }
-    
-    public void moveLeft() {
-    	//position.setLocation(position.getX() - speed, position.getY());
-    	accelX-=0.1;
-    }
     
     public void moveUp() {
     	//position.setLocation(position.getX() ,position.getY() - speed);
