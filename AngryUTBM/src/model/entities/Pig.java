253e0f3b385233794e4ae7d5d8d88cdd130341ed
Dufeu <lois.aubree@orange.fr>
2012-01-09 22:12:09 +0100

    nouvelles collisions

--- AngryUTBM/src/model/entities/Pig.java
+++ AngryUTBM/src/model/entities/Pig.java
@@ -12,10 +12,12 @@ public class Pig extends Enemy {
 
 	private Dimension frameSize;
 	private boolean back;
+	private boolean down;
 	private int time;
+	private int time1;
 	
 	public Pig(int x, int y, float pigSpeed) {
-		super(x,y,26,25);
+		super(x,y,26,26);
 		imagePath = "res/images/pig.png";
 		ImageIcon ii = new ImageIcon(imagePath);
 		image = ii.getImage();
@@ -23,6 +25,7 @@ public class Pig extends Enemy {
 	    imageWidth = ii.getIconWidth();
 		speed = pigSpeed;
 		time = 0;
+		time1 = 0;
 		back = false;
 		frameSize = GameFrame.getFrameSize();
 	}
@@ -37,7 +40,18 @@ public class Pig extends Enemy {
 			hitBox.x += speed;
 		else
 			hitBox.x -= speed;
+		if(down)
+			hitBox.y+=speed;
 		time++;
+		time1++;
+	}
+	public void moveDown(){
+		if(time1 > 10)
+		{
+			down=true;
+			time1=0;
+		}
+		
 	}
 	
 	public void changeDirection() {
@@ -50,6 +64,10 @@ public class Pig extends Enemy {
 
 	public boolean goForward() {
 		return !back;
+	}
+
+	public void doNotMoveDown(){
+		down=false;
 	}
 
 }
